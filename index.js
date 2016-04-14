7200a7f947e77c6084252a36c6ae85ae09b130f0
sanskar-modi <sanskar.modi.civ14@iitbhu.ac.in>
2016-04-15 01:18:53 +0530

    Added connect and disconnect msg

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-	console.log('a user connected');
+	socket.emit('chat message', 'you have connected');
+	socket.broadcast.emit('chat message', 'someone has connected');
+
 	socket.on('disconnect', function(){
-		console.log('user disconnected');
+		socket.broadcast.emit('chat message', 'someone has disconnected');
 	});
 	socket.on('chat message', function(msg){
 		console.log('message', msg);
